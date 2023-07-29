0ee359a252505dfc0aec2d54d5adad98121802b8
Wathsala De Silva <135297355+wattde@users.noreply.github.com>
2023-07-30 00:33:35 +0530

    Add files via upload

---
+++ TutorialsGithub/web tutorials/phptutorialQ1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+
+$x=6;
+$y=4;
+
+$Sum=$x+$y;
+$Dif=$x-$y;
+$Mul=$x*$y;
+$Div=$x/$y;
+echo "Sum:".$Sum."<br>";
+echo "Difference:".$Dif."<br>";
+echo "Product:".$Mul."<br>";
+echo "Division:".$Div."<br>";
+
+
+?>
+
+</body>
+</html>
---
+++ TutorialsGithub/web tutorials/phptutorialQ2ch.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$i;
+
+for ($i=5 ; $i<=15 ; $i+=3){
+echo $i." ";}
+
+
+?>
+
+</body>
+</html>
